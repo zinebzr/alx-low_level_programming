c6d60b2c533a6e9ea06f2d03fb4d9562257ff546
zinebzr <zinebzouhir.zz@gmail.com>
2023-03-20 15:40:37 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		putchar('\n');
+
+	return (0);
+}
