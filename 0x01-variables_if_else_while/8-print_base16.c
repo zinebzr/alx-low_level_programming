001dd8c96fac0cdc75df376d1cf6cbce208e6aee
zinebzr <zinebzouhir.zz@gmail.com>
2023-03-20 17:32:20 -0700

    task8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int d;
+	char ch;
+
+	for (d = '0'; d <= '9'; d++)
+		putchar(d);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
